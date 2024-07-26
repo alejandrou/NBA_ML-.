055c90756d080137d780e5b0bd0b85feb5f42864
alejandrou <adhc1994@gmail.com>
2024-07-26 21:11:14 +0100

    deleted .history files

--- .history/db_manager/db_manager_20240725082525.py
+++
@@ -1,8 +0,0 @@
-#clase intermedia que se encarga de sacar datos de scrap y meterlos en base de datos
-#habria que hacer el import del scrap script y el de la clase team para hacer la insercion, buscar la manera a lo mejor de poder hacerlo con todas las clases
-#futuras aunque lo dudo que se pueda hacer, pero por intentarlo
-
-
-from scrap.scrap_team import scrap_teams
-from models.team import team
-
--- .history/db_manager_20240725074843.py
+++
@@ -1,1 +0,0 @@
-#clase intermedia que se encargue de la conexion a la base de datos y de la insercion de datos

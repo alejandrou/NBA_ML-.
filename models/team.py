556ba197c0b8173c6b640fe06c74d5730c6e0301
alejandro.herreracar <alejandro.herreracar@bitboxonline.es>
2024-07-31 11:48:59 +0100

    it works apparently

--- models/team.py
+++ models/team.py
@@ -1,22 +1,21 @@
 from peewee import Model, CharField, AutoField
 from db_manager.db_conf import db
 
-class Team(Model):
-    id = AutoField()  # Clave primaria
-    Team = CharField()
-    Lg = CharField()
-    From_Year = CharField()
-    To_Year = CharField()
-    Yrs = CharField()
-    G = CharField()
-    W = CharField()
-    L = CharField()
-    WL_Percent = CharField()
-    Plyfs = CharField()
-    Div = CharField()
-    Conf = CharField()
-    Champ = CharField()
-
+class BaseModel(Model):
     class Meta:
         database = db
-        # table_name = 'teams'
+
+class Team(BaseModel):
+    team = CharField()
+    league = CharField()
+    from_year = CharField()
+    to_year = CharField()
+    years = CharField()
+    games = CharField()
+    wins = CharField()
+    losses = CharField()
+    win_loss_percentage = CharField()
+    playoffs = CharField()
+    division = CharField()
+    conference = CharField()
+    championships = CharField()

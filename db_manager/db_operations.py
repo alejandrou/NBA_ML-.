38296f8d5af19a83233e80519c1f071a5a2da461
alejandro.herreracar <alejandro.herreracar@bitboxonline.es>
2024-08-16 13:32:38 +0100

    changed class db_operations, inserts are there, main is simpler

--- db_manager/db_operations.py
+++ db_manager/db_operations.py
@@ -1,9 +1,15 @@
 from db_manager.db_conf import db
+from scrap.scrap_team import TeamScraper
+from scrap.scrap_player import PlayerScraper
+from models.team import Team
+from models.player import Player
+from utils.team_name_abbrev import team_abbrev
 
 class DBOperations():
     
         def __init__(self):
-            pass
+            self.scraper_team = TeamScraper()
+            self.scraper_player = PlayerScraper()
         
         def connect_db(self):
             db.connect()
@@ -16,3 +22,49 @@ class DBOperations():
             
         def drop_tables(self, table):
             db.drop_tables([table])
+        
+        def scrape_and_save_teams(self):
+            team_data = self.scraper_team.get_team_table()
+            self.create_tables(Team)
+            for team in team_data:
+                team_name = team['Team']
+                Team.create(
+                    team_name=team_name,
+                    team_abbreviation=team_abbrev.get(team_name, None),
+                    league=team['Lg'],
+                    from_year=team['From'],
+                    to_year=team['To'],
+                    years=team['Yrs'],
+                    games=team['G'],
+                    wins=team['W'],
+                    losses=team['L'],
+                    win_loss_percentage=team['W/L%'],
+                    playoffs=team['Plyfs'],
+                    division=team['Div'],
+                    conference=team['Conf'],
+                    championships=team['Champ']
+                )
+        
+        
+        async def scrape_and_save_players(self):
+            self.create_tables(Player)
+            teams_data = await self.scraper_player.get_players_team_year()
+
+            for team_name, years_data in teams_data.items():
+                team_instance = Team.get(Team.team_abbreviation == team_name)
+                for year, players_data in years_data.items():
+                    for player in players_data:
+                        player_data = {
+                            'number_player': player.get('No.', None),
+                            'player_name': player.get('Player', None),
+                            'player_position': player.get('Pos', None),
+                            'player_height': player.get('Ht', None),
+                            'player_weight': player.get('Wt', None),
+                            'player_birth_date': player.get('Birth Date', None),
+                            'player_experience': player.get('Exp', None),
+                            'player_college': player.get('College', None),
+                            'player_team': team_name,
+                            'player_year_in_team': year,
+                            'id_team': team_instance.team_id
+                        }
+                        Player.create(**player_data)
--- main.py
+++ main.py
@@ -1,94 +1,20 @@
-from scrap.scrap_team import TeamScraper
-from db_manager.db_operations import DBOperations
-from models.team import Team
-from models.player import Player
-from scrap.scrap_player import PlayerScraper
-from utils.team_name_abbrev import team_abbrev
-
 import asyncio
-
+from db_manager.db_operations import DBOperations
 
 async def main():
-    db_ops = DBOperations()
+    data_manager = DBOperations()
+
     print("Connecting to database...")
-    db_ops.connect_db()
+    data_manager.connect_db()
 
-    scraper_team = TeamScraper()
-    print("Creating team table...")
-    team_df = scraper_team.get_team_table()
-    db_ops.create_tables(Team)
+    print("Creating team table and inserting data...")
+    data_manager.scrape_and_save_teams()
 
-    for team in team_df:
-        team_name = team['Team']
-        Team.create(
-            team_name=team_name,
-            team_abbreviation=team_abbrev.get(team_name, None),
-            league=team['Lg'],
-            from_year=team['From'],
-            to_year=team['To'],
-            years=team['Yrs'],
-            games=team['G'],
-            wins=team['W'],
-            losses=team['L'],
-            win_loss_percentage=team['W/L%'],
-            playoffs=team['Plyfs'],
-            division=team['Div'],
-            conference=team['Conf'],
-            championships=team['Champ']
-        )
+    print("Creating player table and inserting data...")
+    await data_manager.scrape_and_save_players()
 
-    scraper_player = PlayerScraper()
-    print("Creating player table...")
-    db_ops.create_tables(Player)
-    # Supongamos que tienes el diccionario resultante de get_players_team_year()
-    # players_data_by_team = scraper_player.get_players_team_year()
-
-    # for team_name, years_data in players_data_by_team.items():
-    #     team_instance = Team.get(Team.team_abbreviation == team_name)
-    #     for year, players_data in years_data.items():
-    #         for player in players_data:
-    #             player_data = {
-    #                 'number_player': player.get('No.', None),
-    #                 'player_name': player.get('Player', None),
-    #                 'player_position': player.get('Pos', None),
-    #                 'player_height': player.get('Ht', None),
-    #                 'player_weight': player.get('Wt', None),
-    #                 'player_birth_date': player.get('Birth Date', None),
-    #                 'player_experience': player.get('Exp', None),
-    #                 'player_college': player.get('College', None),
-    #                 'player_team': team_name,
-    #                 'player_year_in_team': year,
-    #                 'id_team': team_instance.team_id
-    #             }
-    #             Player.create(**player_data)
-
-    # Ejecutar la obtención de datos
-    print("Iniciando scraping...")
-    teams_data = await scraper_player.get_players_team_year()
-
-    for team_name, years_data in teams_data.items():
-        team_instance = Team.get(Team.team_abbreviation == team_name)
-        for year, players_data in years_data.items():
-            for player in players_data:
-                player_data = {
-                    'number_player': player.get('No.', None),
-                    'player_name': player.get('Player', None),
-                    'player_position': player.get('Pos', None),
-                    'player_height': player.get('Ht', None),
-                    'player_weight': player.get('Wt', None),
-                    'player_birth_date': player.get('Birth Date', None),
-                    'player_experience': player.get('Exp', None),
-                    'player_college': player.get('College', None),
-                    'player_team': team_name,
-                    'player_year_in_team': year,
-                    'id_team': team_instance.team_id
-                }
-                Player.create(**player_data)
-
-    # db_ops.drop_tables(Team)
     print("Closing database connection...")
-    db_ops.close_db()
-
+    data_manager.close_db()
 
 if __name__ == "__main__":
     asyncio.run(main())

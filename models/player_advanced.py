62af833c461d82da782fd4dfbf47ec5d40f97913
alejandro.herreracar <alejandro.herreracar@bitboxonline.es>
2024-08-28 12:09:08 +0100

    starting advanced table

---
+++ models/player_advanced.py
@@ -0,0 +1,38 @@
+from peewee import Model, CharField, IntegerField, ForeignKeyField, AutoField
+from db_manager.db_conf import db
+from models.player import Player
+
+class PlayerAdvanced(Model):
+    
+    id_player_stat_advanced = AutoField(primary_key=True)
+    id_player = ForeignKeyField(Player, to_field="id_player")
+    rk = CharField()
+    player = CharField()
+    age = IntegerField()
+    games = IntegerField()
+    minutes_played = IntegerField()
+    player_effiencey_rating = CharField()
+    true_shooting_percentage = CharField()
+    three_point_attempt_rate = CharField()
+    free_throw_attempt_rate = CharField()
+    offensive_rebound_percentage = CharField()
+    defensive_rebound_percentage = CharField()
+    total_rebound_percentage = CharField()
+    assist_percentage = CharField()
+    steal_percentage = CharField()
+    block_percentage = CharField()
+    turnover_percentage = CharField()
+    usage_percentage = CharField()
+    offensive_win_shares = CharField()
+    defensive_win_shares = CharField()
+    win_shares = CharField()
+    win_shares_per_48_minutes = CharField()
+    offensive_box_plus_minus = CharField()
+    defensive_box_plus_minus = CharField()
+    box_plus_minus = CharField()
+    value_over_replacement_player = CharField()
+    
+    class Meta:
+        database = db
+        schema = 'players'
+        table_name = 'player_advanced'
---
+++ scrap/scrap_player_advanced.py
@@ -0,0 +1,66 @@
+import aiohttp
+import asyncio
+from bs4 import BeautifulSoup
+
+class PlayerScraperRoster:
+    
+    def __init__(self):
+        self.url = 'https://www.basketball-reference.com/teams/'
+        self.teams_NBA_list = ['ATL']
+        self.years = ['2024']
+
+    async def fetch(self, session, url):
+        print(f"Fetching URL: {url}")
+        async with session.get(url) as response:
+            content = await response.text()
+            print(f"Finished fetching URL: {url}")
+            return content
+
+    async def scrape_team_year_roster(self, session, nba_team, year):
+        print(f"Starting scrape for {nba_team} in {year}")
+        url = f'https://www.basketball-reference.com/teams/{nba_team}/{year}.html'
+        html_content = await self.fetch(session, url)
+        soup = BeautifulSoup(html_content, 'html.parser')
+        table = soup.find('table', {'id': 'advanced'})
+        
+        if table:
+            print(f"Found roster table for {nba_team} in {year}")
+            headers = [th.text.strip() for th in table.find('thead').find_all('th')]
+            rows = [
+                {headers[i]: cell.text.strip() for i, cell in enumerate(tr.find_all(['th', 'td']))}
+                for tr in table.find('tbody').find_all('tr')
+            ]
+            print(f"Scraping complete for {nba_team} in {year}")
+            return rows
+        else:
+            print(f"No roster table found for {nba_team} in {year}")
+            return []
+
+    async def get_players_team_year_roster(self):
+        print("Starting to get players for all teams and years")
+        dictionary_of_teams = {}
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            for nba_team in self.teams_NBA_list:
+                dictionary_of_teams[nba_team] = {}
+                for year in self.years:
+                    print(f"Queueing task for {nba_team} in {year}")
+                    task = asyncio.create_task(self.scrape_team_year_roster(session, nba_team, year))
+                    dictionary_of_teams[nba_team][year] = task
+                    tasks.append(task)
+
+            print("All tasks queued, waiting for completion...")
+            results = await asyncio.gather(*tasks)
+            print("All tasks completed.")
+
+            for i, nba_team in enumerate(self.teams_NBA_list):
+                for j, year in enumerate(self.years):
+                    dictionary_of_teams[nba_team][year] = results[i * len(self.years) + j]
+
+            for team, years in dictionary_of_teams.items():
+                for year, players in years.items():
+                    for player in players:
+                        dictionary_of_teams[team][year] = {k : v for k, v in player.items() if k != ''}
+            
+        print("Finished getting players for all teams and years")
+        return dictionary_of_teams

4cb978e7c9e518152fccabbe899c2cfa0ff960c7
alejandrou <adhc1994@gmail.com>
2025-01-29 00:41:02 +0000

    reestructure of db_manager

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 from db_manager.db_manager import DBManager
 from db_manager.team_operations.team_operations import TeamOperations
-from db_manager.player_operations import PlayerOperations
+from db_manager.player_operations.player_operations import PlayerOperations
 import asyncio
 import httpx
-import time
-from datetime import datetime
-import argparse
 import sys
 
 def parse_years():
-    if len(sys.argv) == 1:  # No arguments provided
-        return [2024]  # Default year
+    if len(sys.argv) == 1:
+        return [2024]
     return [int(year) for year in sys.argv[1:]]  
 
 async def main():
     years = parse_years()
-    start_time = time.time()
     data_manager = DBManager()
     player_operations = PlayerOperations(years)
     team_operations = TeamOperations(years)
@@ -24,16 +20,15 @@ async def main():
     data_manager.create_schemas('teams')
     data_manager.create_schemas('players')
 
-    # print("Creating team table and inserting data...")
-    # team_operations.insert_teams()
+    print("Creating team table and inserting data...")
     async with httpx.AsyncClient() as client:
-        # print("Scraping and saving team season results...")
+        print("Scraping and saving team season results...")
         await team_operations.get_regular_season_results_data(client)
-        # print("Scraping and saving player rosters...")
+        print("Scraping and saving player rosters...")
         await player_operations.scrape_and_save_players_roster_async(client)
-        # print("Scraping and saving player totals...")
+        print("Scraping and saving player totals...")
         await player_operations.scrape_and_save_players_totals_async(client)
-        # print("Scraping and saving player advanced...")
+        print("Scraping and saving player advanced...")
         await player_operations.scrape_and_save_players_advanced(client)
  
     #To do
@@ -52,21 +47,12 @@ async def main():
     
     #add injury table
     
-    
-    
-    # print("Creating player table and inserting data...")
-    # await player_operations.scrape_and_save_players_roster()
-    # await player_operations.scrape_and_save_players_advanced()
-
     # print("Dropping schemas...")
     # data_manager.drop_schemas('teams')
     # data_manager.drop_schemas('players')
     
     print("Closing database connection...")
     data_manager.close_db()
-    total_time = time.time() - start_time
-    print(f"Total time taken: {total_time} seconds")
-    print(f"Script finished at: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
 if __name__ == "__main__":
     asyncio.run(main())
